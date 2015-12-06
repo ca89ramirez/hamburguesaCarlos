0a4a785679af47958e0f3a1ea35ff84f7bbb095f
Carlos Ramirez <cramirez@ozelot.it>
2015-12-06 16:45:15 -0500

    Initial Commit

---
+++ HamburguesasCarlos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburguesasCarlos
+//
+//  Created by Carlos Ramirez Velazquez on 12/6/15.
+//  Copyright © 2015 Carlos Ramirez Velazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
